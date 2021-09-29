9f65d3cf2363a67397c881d1d760a76e8ddf5422
christianturner <christianturner@hey.com>
2021-09-29 13:30:00 -0400

    italic fix

--- pages/components/Tip.tsx
+++ pages/components/Tip.tsx
@@ -3,13 +3,16 @@ import React from "react";
 
 const Tip = () => {
   return (
-    <div className=" w-auto">
+    <div className="w-auto xs:px-4">
       <img className="m-auto" src="/lines/toadz-line-2.gif" alt="line2" />
 
       <div className="text-center">
-        <p className="py-2 italic font-bold text-xl">
-          Want to support ongoing development? Tip jar:{" "}
-          <span className="text-scratchy address">christiantuner.eth</span>
+        <p className="py-2 italic font-bold xs:text-md  sm:text-xl">
+          Want to support ongoing development?
+          <br /> <span className="not-italic	">Tip jar:</span>{" "}
+          <span className="text-scratchy not-italic address">
+            christiantuner.eth
+          </span>
         </p>
       </div>
 
