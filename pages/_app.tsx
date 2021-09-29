eca2058a8557941fded7c62de023c13b852c05fd
christianturner <christianturner@hey.com>
2021-09-29 15:54:10 -0400

    styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import "react-modal-video/scss/modal-video.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
