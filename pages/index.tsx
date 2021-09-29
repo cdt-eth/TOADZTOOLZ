05bc10a67c16d6b51d678acf5fc668cb3e5aab91
christianturner <christianturner@hey.com>
2021-09-29 06:09:07 -0400

    homepage changes, variable renaming, and breaking pieces out into components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
-import FormModal from "./FormModal";
+import FormModal from "./components/FormModal";
 import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Tip from "./components/Tip";
 
 export default function Home() {
   const [id, setId] = useState(3470);
-  const [lootObject, setLootObject] = useState({});
+  const [sockzData, setSockzData] = useState({});
 
   useEffect(() => {
     checkSockz(id);
@@ -24,32 +27,35 @@ export default function Home() {
 
       const data = await fetchResponse.json();
 
-      setLootObject(data);
+      setSockzData(data);
     } catch {
       console.error("Error fetching loot information");
     }
   };
 
-  console.log("lootobject", lootObject);
-
   return (
-    <div className="container">
+    <div className=" ">
       <Head>
-        <title>ToadzVerse</title>
+        <title>TOADZTOOLZ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to the <span>Lily Pad</span>
-        </h1>
-        <div className="grid">
+      <main className="p-3">
+        <Header />
+
+        <div className="flex py-12 md:w-3/4">
           <FormModal
-            lootObject={lootObject}
+            sockzData={sockzData}
             sockzId={id}
             checkSockz={checkSockz}
           />
+          <p className="self-center sm:text-4xl copy">
+            Click and provide a Cryptoadz id to see if your Sockz have been
+            claimed.
+          </p>
+        </div>
 
+        {/* <div className="grid">
           <a
             href="https://cryptoadz.io/"
             className="card"
@@ -79,187 +85,12 @@ export default function Home() {
             <h3>Cryptoadz Smart Contract &rarr;</h3>
             <p>Learn more by seeing exploring the deployed smart contract.</p>
           </a>
-        </div>
+        </div> */}
       </main>
 
-      <footer>
-        <div className="credit">
-          <div>
-            Forked from
-            <a
-              href="https://twitter.com/mehranhydary"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="credit"
-            >
-              @mehranhydary
-            </a>
-          </div>
-          <div>
-            Built by
-            <a
-              href="https://twitter.com/cdt_works"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="credit"
-            >
-              @cdt_works
-            </a>
-          </div>
-        </div>
-      </footer>
+      <Tip />
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer a {
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-          cursor: pointer;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-
-        .credit {
-          text-align: center;
-        }
-        .credit a {
-          padding-left: 3px;
-        }
-        .credit a:hover {
-          color: #0070f3;
-          border-color: #0070f3;
-          cursor: pointer;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <Footer />
     </div>
   );
 }
