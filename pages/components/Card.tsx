d06c0196984c6441a421697a95284ab43e1994f2
christianturner <christianturner@hey.com>
2021-10-05 16:44:30 -0400

    add open in new page prop and link to internal Toolz page

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -4,13 +4,19 @@ interface CardProps {
   link: string;
   title: string;
   text: string;
+  newPage: boolean;
 }
 
-const Card = ({ link, title, text }: CardProps) => {
+const Card = ({ link, title, text, newPage }: CardProps) => {
   return (
-    <a href={link} className="card " target="_blank" rel="noreferrer">
+    <a
+      href={link}
+      className="card "
+      target={newPage ? "_blank" : "_self"}
+      rel="noreferrer"
+    >
       <h3 className="text-scratchy">{title} </h3>
-      {/* <h3 className="text-scratchy">{title} &rarr;</h3> */}
+
       <p className="text-md">{text}</p>
     </a>
   );
--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -5,31 +5,37 @@ const Projects = () => {
   return (
     <div className="items flex m-auto sm:py-10 sm:pt-2 xs:text-lg">
       <Card
+        newPage={false}
         link="/toolz"
-        title="Derivatives"
-        text="See what the Toadz community is building!"
+        title="Toolz"
+        text="Full list of resources & projects from the Toadz!"
       />
       <Card
+        newPage={true}
         link="https://www.ghosts.market/"
         title="Ghosts Market"
         text="Marketplace for Ghost Cryptoadz"
       />
       <Card
+        newPage={true}
         link="https://www.sockz.exchange/"
         title="Sockz Exchange"
         text="Claim your sockz here!"
       />
       <Card
+        newPage={true}
         link="https://www.partybid.app/party/0xE43f0Bf1Fc7B5f8FA3c29b4B62294CA996C93D93"
         title="PartyGhost"
         text="GhostsDAO's partybid on the rarest Ghost Toad"
       />
       <Card
+        newPage={true}
         link="https://opensea.io/collection/cryptoadz-by-gremplin"
         title="Buy Toadz"
         text="Shop for Cryptoadz on OpenSea!"
       />
       <Card
+        newPage={true}
         link="https://etherscan.io/address/0x1cb1a5e65610aeff2551a50f76a87a7d3fb649c6"
         title="Etherscan"
         text="Learn more by seeing, explore the deployed smart contract."
