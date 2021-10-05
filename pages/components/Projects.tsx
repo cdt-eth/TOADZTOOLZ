67504082a8033577d59790ebe2a13f1721359794
christianturner <christianturner@hey.com>
2021-10-05 16:47:53 -0400

    hide old links to showcase new Hero Toolz page

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -4,13 +4,7 @@ import Card from "./Card";
 const Projects = () => {
   return (
     <div className="items flex m-auto sm:py-10 sm:pt-2 xs:text-lg">
-      <Card
-        newPage={false}
-        link="/toolz"
-        title="Toolz"
-        text="Full list of resources & projects from the Toadz!"
-      />
-      <Card
+      {/* <Card
         newPage={true}
         link="https://www.ghosts.market/"
         title="Ghosts Market"
@@ -27,12 +21,18 @@ const Projects = () => {
         link="https://www.partybid.app/party/0xE43f0Bf1Fc7B5f8FA3c29b4B62294CA996C93D93"
         title="PartyGhost"
         text="GhostsDAO's partybid on the rarest Ghost Toad"
-      />
+      /> */}
       <Card
         newPage={true}
         link="https://opensea.io/collection/cryptoadz-by-gremplin"
         title="Buy Toadz"
         text="Shop for Cryptoadz on OpenSea!"
+      />
+      <Card
+        newPage={false}
+        link="/toolz"
+        title="Toolz"
+        text="Full list of resources & projects from the Toadz!"
       />
       <Card
         newPage={true}
