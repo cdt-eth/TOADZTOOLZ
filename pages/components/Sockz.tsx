4f2b2a1cbc2d72ed97fa63baa7edf78194191efe
christianturner <christianturner@hey.com>
2021-10-07 00:57:45 -0400

    fix spacing

--- pages/components/Sockz.tsx
+++ pages/components/Sockz.tsx
@@ -3,7 +3,7 @@ import FormModal from "./FormModal";
 
 const Sockz = () => {
   return (
-    <div className="flex xs:py-7 sm:py-6 md:w-3/4 m-auto xs:px-1">
+    <div className="flex xs:py-7 sm:py-2  md:w-3/4 m-auto xs:px-1">
       <FormModal />
 
       <p className="self-center xs:text-2xl sm:text-4xl copy">
