da9305483b84d647a10e6b51650dda7be649e91f
christianturner <christianturner@hey.com>
2021-10-22 21:40:08 -0400

    rename to Claim Center

---
+++ pages/components/ClaimCenter.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FormModal from "./FormModal";
+
+const ClaimCenter = () => {
+  return (
+    <div className="flex xs:py-7 sm:py-2 md:w-3/4 m-auto xs:px-2">
+      <FormModal />
+
+      <p className="self-center xs:text-xl sm:text-4xl copy">
+        Check and see if your exclusive Toad-holder NFTs have been claimed!
+      </p>
+    </div>
+  );
+};
+
+export default ClaimCenter;
