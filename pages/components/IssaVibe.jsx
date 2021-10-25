4502638f342ef41d055f1c2b0a4cf9ac2f742b0a
christianturner <christianturner@hey.com>
2021-10-25 14:12:25 -0400

    change song

--- pages/components/IssaVibe.jsx
+++ pages/components/IssaVibe.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactAudioPlayer from "react-audio-player";
 
 const IssaVibe = () => {
-  return <ReactAudioPlayer src="issa_vibe.mp3" autoPlay loop />;
+  return <ReactAudioPlayer src="vibe.mp3" autoPlay loop />;
 };
 
 export default IssaVibe;
