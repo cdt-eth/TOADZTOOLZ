3995251f413cb4724d2f6d90a78d563cc57f6d4b
christianturner <christianturner@hey.com>
2022-02-12 09:45:27 -0500

    rm comments

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -52,36 +52,3 @@ export default function Projects() {
     </div>
   );
 }
-
-// const Projects = () => {
-//   return (
-//     <div className="items flex m-auto sm:py-10 sm:pt-2 xs:text-lg">
-//       <Card
-//         newPage={false}
-//         link="/toolz"
-//         title="Toolz"
-//         text="Full list of resources & projects from the Toadz!"
-//       />
-//       <Card
-//         newPage={false}
-//         link="/buildz"
-//         title="Buildz"
-//         text="Create your own Cryptoadz with the new Toad Builder."
-//       />
-//       <Card
-//         newPage={false}
-//         link="/filez"
-//         title="Filez"
-//         text="Download all Cryptoadz assets."
-//       />
-//       <Card
-//         newPage={false}
-//         link="/ideaz"
-//         title="Ideaz"
-//         text="Submit your project or idea for the site here!"
-//       />
-//     </div>
-//   );
-// };
-
-// export default Projects;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
-import Banner from "./components/Banner";
 import Credit from "./components/Credit";
 import Projects from "./components/Projects";
-import ClaimCenter from "./components/ClaimCenter";
 import Header from "./components/Header";
 import dynamic from "next/dynamic";
-import Vibe from "./components/Vibe";
-import FloorToadz from "./components/FloorToadz";
 import { useTheme } from "next-themes";
 import Nav from "./components/Nav";
-// import ToadTicker from "./components/ToadTicker";
 
 const Roadmap = dynamic(() => import("../Toadmap"), {
   ssr: false,
@@ -28,15 +23,8 @@ export default function Home() {
       <div className="bg-toadz-lightgray w-full dark:bg-toadz-dark h-screen">
         <Header title="TOADZTOOLZ" />
 
-        {/* <Nav /> */}
-        {/* <Vibe /> */}
-        {/* <Banner /> */}
         <Projects />
-        {/* <div className="hidden sm:block">
-          <FloorToadz />
-        </div> */}
-        {/* <ClaimCenter /> */}
-        {/* {theme === "light" ? null : <Roadmap />} */}
+
         <Credit />
       </div>
     </>
